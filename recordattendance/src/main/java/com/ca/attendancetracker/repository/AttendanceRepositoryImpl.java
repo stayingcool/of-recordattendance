86ff914699bb8ad82dfda9e5f10c972bb016f365
NISAR AHMED <stayingcool@gmail.com>
2018-09-16 21:54:42 +0530

    yaml change for gateway and img

--- recordattendance/src/main/java/com/ca/attendancetracker/repository/AttendanceRepositoryImpl.java
+++ recordattendance/src/main/java/com/ca/attendancetracker/repository/AttendanceRepositoryImpl.java
@@ -33,8 +33,8 @@ public class AttendanceRepositoryImpl implements AttendanceRepository {
     public List<Attendance> getAttendance() {
         Attendance attendance = new Attendance();
         attendance.setName("Corner Canyon");
-        attendance.setId("somseID");
-        attendance.setUserid("ahsmni01");
+        attendance.setId("TestID");
+        attendance.setUserid("ahmni01");
         List<Attendance> attendances = new ArrayList<>();
         attendances.add(attendance);
         return attendances;
